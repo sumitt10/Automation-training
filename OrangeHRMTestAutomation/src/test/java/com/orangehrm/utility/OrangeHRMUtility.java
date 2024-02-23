c05526054a5817a2cf1ec7dce41e59c4708b610b
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-23 15:24:57 +0530

    Updated Utility Method - getDataAndCompare

--- OrangeHRMTestAutomation/src/test/java/com/orangehrm/utility/OrangeHRMUtility.java
+++ OrangeHRMTestAutomation/src/test/java/com/orangehrm/utility/OrangeHRMUtility.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.By; 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
