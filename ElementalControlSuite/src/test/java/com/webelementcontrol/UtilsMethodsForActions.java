41dc72ffa33917526dc247ff0b1fe93bc29e8a0f
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-27 19:18:38 +0530

    Created Utils methods to Perform Action on Elements

--- ElementalControlSuite/src/test/java/com/webelementcontrol/UtilsMethodsForActions.java
+++ ElementalControlSuite/src/test/java/com/webelementcontrol/UtilsMethodsForActions.java
@@ -11,12 +11,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * Utility class containing methods to perform various actions on web elements.
  */
-@Slf4j
+
 public class UtilsMethodsForActions {
 
 	private WebDriver driver;
@@ -30,40 +28,6 @@ public class UtilsMethodsForActions {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	}
-
-	public static void main(String[] args) {
-		UtilsMethodsForActions utils = new UtilsMethodsForActions();
-
-		// Open the Webpage for automate
-		utils.openURL("https://testpages.eviltester.com/styled/basic-html-form-test.html");
-
-		// Enter text in the username field
-		utils.enterText(By.name("username"), "John");
-		log.info("Entered the text in specied field.");
-
-		// Check if the submit button is present
-		boolean isSubmitButtonPresent = utils.isElementPresent(By.name("submitbutton"));
-		log.info("Is Submit Button Present: {}", isSubmitButtonPresent);
-
-		// Select a radio button
-		utils.selectRadioButton(By.xpath("/html/body/div/div[3]/form/table/tbody/tr[6]/td/input[3]"));
-		log.info("Radio button selected successfully.");
-
-		// Perform a mouse hover action
-		utils.mouseHover(By.xpath("/html/body/div/div[1]/div[1]/a"));
-		log.info("Mouse hover action performed successfully.");
-
-		// Select an item from a dropdown
-		utils.selectDropdownByVisibleText(By.xpath("/html/body/div/div[3]/form/table/tbody/tr[8]/td/select"),
-				"Drop Down Item 6");
-		log.info("Dropdown item selected successfully.");
-
-		// Click on a visible element using partial link text
-		utils.clickOnVisibleElement(By.partialLinkText("Index"));
-		log.info("Clicked on the element with partial link text.");
-
-		utils.quit();
 	}
 
 	/**
