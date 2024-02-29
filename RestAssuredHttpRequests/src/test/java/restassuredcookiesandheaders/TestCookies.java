b42f6a165568179d844d7f7f78074d3af203a9fc
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-29 10:28:53 +0530

    Updated Code

--- RestAssuredHttpRequests/src/test/java/restassuredcookiesandheaders/TestCookies.java
+++ RestAssuredHttpRequests/src/test/java/restassuredcookiesandheaders/TestCookies.java
@@ -3,20 +3,22 @@ package restassuredcookiesandheaders;
 import static io.restassured.RestAssured.given;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.testng.annotations.Test;
 
 import io.restassured.response.Response;
 
 public class TestCookies {
+	private static final Logger log = Logger.getLogger(TestCookies.class.getName());
 
 	@Test
 	void getCookieInfo() {
 		Response res = given().when().get("https://www.google.com/");
 		String cookie_value = res.getCookie("AEC");
-		System.out.println("Value of Cookie is " + cookie_value);
+		log.info("Value of Cookie is " + cookie_value);
 
 		Map<String, String> cookies_values = res.getCookies();
-		System.out.println(cookies_values.keySet());
+		log.info("Set of Cookies names :" + cookies_values.keySet());
 	}
 }
