d7374d882b8b1ec8f5360360bf01c4ddea15202c
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-29 10:30:08 +0530

    Testing API - HTTP Requests,Logs,Cookies & Logs

--- RestAssuredHttpRequests/src/test/java/restassuredcookiesandheaders/TestHeaders.java
+++ RestAssuredHttpRequests/src/test/java/restassuredcookiesandheaders/TestHeaders.java
@@ -2,11 +2,14 @@ package restassuredcookiesandheaders;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.logging.Logger;
+
 import org.testng.annotations.Test;
 
 import io.restassured.response.Response;
 
 public class TestHeaders {
+	private static final Logger log = Logger.getLogger(TestCookies.class.getName());
 
 	@Test(priority = 1)
 	void testHeaders() {
@@ -18,6 +21,6 @@ public class TestHeaders {
 	void getHeaders() {
 		Response res = given().when().get("https://www.google.com/");
 		String headervalue = res.getHeader("Content-Type");
-		System.out.println("The value of Header is " + headervalue);
+		log.info("The value of Header is " + headervalue);
 	}
 }
