46b306d27c192cb954c5b5ad84348c7e7dea7a64
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-03-02 21:02:56 +0530

    OrangeHRM- Develop and automate test cases for Attendance Tracking

---
+++ AttendanceTrackingTestAutomation/src/test/java/com/utilities/OrangeHRMAutoLogin.java
@@ -0,0 +1,50 @@
+package com.utilities;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import lombok.extern.slf4j.Slf4j;
+
+/*
+ * Section : OrangeHRM - Login
+ * Description : This class facilitates the automated login process for the OrangeHRM website.
+ * 
+ */
+
+@Slf4j
+public class OrangeHRMAutoLogin {
+
+	private WebDriver driver;
+
+	public OrangeHRMAutoLogin(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void login() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+		driver.manage().window().maximize();
+
+		String username = "Admin";
+		String password = "admin123";
+
+		WebElement usernameField = wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//input[contains(@class, 'oxd-input')][@name='username']")));
+		WebElement passwordField = wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//input[contains(@class, 'oxd-input')][@name='password']")));
+		WebElement loginButton = wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[contains(@class, 'orangehrm-login-button')]")));
+
+		usernameField.sendKeys(username);
+		passwordField.sendKeys(password);
+		loginButton.click();
+		log.info("Successfully logged in OrangeHRM webpage!!");
+	}
+}
